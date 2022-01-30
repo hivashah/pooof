c9d5fbab41dfbe0bdc2ee43e2bde1c39546716fc
hivashah <76765644+hivashah@users.noreply.github.com>
2022-01-30 10:53:45 +0330

    Add files via upload

--- Undo.java
+++ Undo.java
@@ -1,19 +1,24 @@
 public class Undo {
-    public static void undo(){
+    public static void undo() {
         String lastCommand = LineReader.stack.peek();
         String[] strings;
 
         strings = lastCommand.trim().split("[ ]+");
         LineReader.stack.pop();
 
-        if (strings[0].equals("new")){
-            Rope.ropes.remove(Rope.ropes.size()-1);
-        }else if (strings[0].equals("split")){
+        if (strings[0].equals("new")) {
+            Rope.ropes.remove(Rope.ropes.size() - 1);
+        } else if (strings[0].equals("split")) {
             int s = Integer.parseInt(strings[1]);
-            Rope.concat(Rope.ropes.get(s) , Rope.ropes.get(s+1));
-        }else if (strings[0].equals("concat")){
-            int length = strings[1].length()-1;
-            Rope.splitInMethod(Rope.ropes.get(Integer.parseInt(strings[1])),length);
+            Rope.concat(Rope.ropes.get(s), Rope.ropes.get(s + 1));
+        } else if (strings[0].equals("concat")) {
+            int length =Rope.ropes.get(Integer.parseInt(strings[1])).value;
+            Rope.splitInMethod(Rope.ropes.get(Integer.parseInt(strings[1])), length);
+        }
+        else if (strings[0].equals("insert")){
+
         }
     }
+    
 }
+
